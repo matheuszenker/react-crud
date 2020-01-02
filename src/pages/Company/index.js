8c6d781aee286305d14377cb06c7adf4f65a6d3d
Matheus Zenker <matheus.zenker@cigam.com.br>
2020-01-02 13:30:59 -0300

    Componente criado

---
+++ src/pages/Company/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Crud from '../../components/Crud';
+
+const Company = () => {
+  return (
+    <>
+      <div>
+        <h2>Fique por Dentro</h2>
+      </div>
+      <Crud />
+    </>
+  );
+};
+
+export default Company;
