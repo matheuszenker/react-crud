6962b512b758ccedfe9cc8afb201488c8592e2fc
Matheus Zenker <matheus.g.zenker@gmail.com>
2020-02-09 15:50:03 -0300

    Changed number of rows that display.

--- src/components/Crud/components/Table/index.js
+++ src/components/Crud/components/Table/index.js
@@ -13,34 +13,36 @@ import MaterialUI from '../MaterialUI';
 
 import { Container } from './styles';
 
-const Table = ({ handleForm, structure, data, nextPage, previousPage }) => (
-  <Container>
-    <MaterialUI stickyHeader />
-    <div>
-      <Button
-        startIcon={<AddIcon />}
-        variant="contained"
-        color="primary"
-        onClick={handleForm}
-      >
-        Add
-      </Button>
-    </div>
-    <TableUI size="small">
-      <TableHeader structure={structure} />
-      <TableBody data={data} structure={structure} />
-    </TableUI>
-    <TablePagination
-      rowsPerPageOptions={[5, 10, 25]}
-      component="div"
-      count={100}
-      rowsPerPage={5}
-      page={0}
-      onChangePage={() => {}}
-      onChangeRowsPerPage={() => {}}
-    />
-  </Container>
-);
+const Table = ({ handleForm, structure, data, nextPage, previousPage }) => {
+  return (
+    <Container>
+      <MaterialUI stickyHeader />
+      <div>
+        <Button
+          startIcon={<AddIcon />}
+          variant="contained"
+          color="primary"
+          onClick={handleForm}
+        >
+          Add
+        </Button>
+      </div>
+      <TableUI size="small">
+        <TableHeader structure={structure} />
+        <TableBody data={data} structure={structure} />
+      </TableUI>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={100}
+        rowsPerPage={5}
+        page={0}
+        onChangePage={() => {}}
+        onChangeRowsPerPage={() => {}}
+      />
+    </Container>
+  );
+};
 
 Table.propTypes = {
   handleForm: PropTypes.func.isRequired,
